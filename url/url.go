1739aa248e0fd54fc7128e58181c47ddfd19ea58
ghaidafasya24 <ghaidafasya5@gmail.com>
2024-12-19 14:43:39 +0700

    add CRUD operations for product management

--- url/url.go
+++ url/url.go
@@ -15,16 +15,17 @@ func Web(page *fiber.App) {
 	page.Patch("/", controller.Sink)
 	page.Delete("/", controller.Sink)
 	page.Options("/", controller.Sink)
-	page.Post("/users/register", controller.Register)
-	page.Post("/users/login", controller.Login)			
-	
-
 	page.Get("/checkip", controller.Homepage) //ujicoba panggil package musik
-	page.Get("/product", controller.GetAllProduct)
-	// page.Get("/product/:id", controller.GetMenuID) //menampilkan data menu berdasarkan id
-	page.Post("/insert/product", controller.InsertDataProduct)
-	// page.Put("/update/:id", controller.UpdateData)
-	// page.Delete("/delete/:id", controller.DeleteMenuByID)
+	// AUTH
+	page.Post("/users/register", controller.Register)
+	page.Post("/users/login", controller.Login)
+
+	// PRODUCT
+	page.Post("/insert/product", controller.InsertDataProduct)        //menambahkan data product
+	page.Get("/product", controller.GetAllProduct)                    //menampilkan semua data product
+	page.Get("/product/:id", controller.GetProductID)                 //menampilkan data product berdasarkan id
+	page.Put("/update/product/:id", controller.UpdateDataProduct)     //update data product
+	page.Delete("/product//delete/:id", controller.DeleteProductByID) //delete data product
 
 	// page.Get("/docs/*", swagger.HandlerDefault)
 }
