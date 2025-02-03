5dcb9062452638b97a7cc36ca422784c20d0afde
ghaidafasya24 <ghaidafasya5@gmail.com>
2025-02-03 10:33:58 +0700

    Add a blank line for improved code readability in auth.go

--- controller/auth.go
+++ controller/auth.go
@@ -18,6 +18,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+
 )
 
 var jwtKey = []byte("secret_key!234@!#$%")
